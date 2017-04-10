91107027fa13a5b9311220640deea62e67c004fa
gretchenriggs <gretchen.m.riggs@gmail.com>
2017-04-10 09:10:43 -0600

    Adding MongoDB updates

--- Cure_sent_analysis.py
+++ Cure_sent_analysis.py
@@ -2,23 +2,46 @@ import requests
 from bs4 import BeautifulSoup as bs
 import urllib3.request
 from pymongo import MongoClient
+import sys
+# import spacy
+from time import sleep
+import pandas as pd
+
+# Load spacy.en if not loaded already
+# If using ipython, execute script using %run -i my_file.py to avoid
+#   repeatedly loading in the English module
+# if not 'nlp' in locals():
+#     print("Loading English Module...")
+#     nlp = spacy.load('en')
 
 
-# Creating band's url page name on azlyrics.com
 def band_url_generator(band):
-    # If band name starts with "the", remove it
+    ''' Creating band's url page name on azlyrics.com
+        Input:  band      string, raw input from user
+        Output: band      string, reformatted band name
+                band_url  string, band url on azlyrics.com
+    '''
+    # Format band name as lower case with no space and if band name starts
+    #   with "the", remove it
+    band = band.lower()
     if band.split(" ")[0] == "the":
         band = band[4:]
-
+    band = band.replace(" ", "")
     # Generate band_url for page on azlyrics.com containing all band's
     #   songs
     band_url = "http://www.azlyrics.com/[first_letter]/[band].html"
     band_url = band_url.replace("[first_letter]", band[0])\
                        .replace('[band]', band)\
                        .replace(" ", "")
-    return band_url
+    return band, band_url
+
 
 def song_list_generator(band_url):
+    ''' Creating list of songs for band from azlyrics.com
+        Input: band_url     string, url for band on azlyrics.com
+        Output: song_list   list of strings, list containing all songs with
+                                  lyrics for specified band on azlyrics.com
+    '''
     # Creating a PoolManager, which is an abstraction for a container for a
     #   collection of connections to the host
     http = urllib3.PoolManager()
@@ -60,6 +83,11 @@ def song_list_generator(band_url):
 
 
 def scrape_lyrics(band, song_list):
+    ''' Scraping lyrics from all songs listed for band on azlyrics.com
+        Input: band          string, band name
+               song_list     list of strings, list of song names
+        Output: collection   MongoDB, containing song names and lyrics
+    '''
     # Creating mongo database_name & collection_name
     database_name = band +'_lyrics'
     collection_name = 'lyrics'
@@ -74,8 +102,20 @@ def scrape_lyrics(band, song_list):
         #   collection of connections to the host
         http = urllib3.PoolManager()
 
-        # Make a request to the host
-        response  = http.request('GET', lyrics_url)
+        # Making sure to have enough time between requests so not hitting
+        #   max requests to website
+        response = ""
+        while response == "":
+            try:
+                # Make a request to the host
+                response  = http.request('GET', lyrics_url)
+            except:
+                # Print messages to screen and sleep for 5 seconds and try again
+                print "Connection refused by the server."
+                print "Sleeping for 5 seconds."
+                sleep(5)
+                print "Continuing"
+                continue
 
         # If request succeeded, proceed with lyric scraping
         if response.status == 200:
@@ -101,18 +141,26 @@ def scrape_lyrics(band, song_list):
             song_lyrics = {song: lyrics}
 
             # Insert song and lyrics into mongo database
-            db_insert_lyrics(database_name, collection_name, song_lyrics)
+            # if db
+            db_insert_lyrics(database_name, collection_name, song, song_lyrics)
 
         else:
             error_message = "Lyrics could not be found"
-            db_insert_lyrics(database_name, collection_name, error_message)
+            db_insert_lyrics(database_name, collection_name, song, \
+                             error_message)
 
+        sleep(5)
     # return collection for further QC
     collection = connect_to_mongo(database_name, collection_name)
     return collection
 
 
 def connect_to_mongo(database_name, collection_name):
+    ''' Connect to Mongo database
+        Input: database_name      string, MongoDB database name
+               collection_name    string, MongoDB collection name
+        Output: lyric_collection  database table, connecting to MongoDB table
+    '''
     # Initial client to connect to mongo
     client = MongoClient()
 
@@ -123,21 +171,59 @@ def connect_to_mongo(database_name, collection_name):
     lyric_collection = database[collection_name]
     return lyric_collection
 
+
 def db_insert_lyrics(database_name, collection_name, lyrics):
+    ''' Insert lyrics for song into MongoDB table
+        Input: database_name    string, MongoDB database name to insert into
+               collection name  string, MongoDB collection name to insert into
+               lyrics           string, lyrics to insert into MongoDB
+        Output: None, collection is just updated
+    '''
     # Connect to and initialize mongo database
     collection = connect_to_mongo(database_name,collection_name)
 
     # Insert new song lyrics into mongo database
-    collection.insert_one(lyrics)
+    collection.insert_one({song : lyrics})
+
+
+def lyrics_to_csv(collection, band, query=None):
+    ''' Output lyric collection for band to csv file
+        Input: collection   string, MongoDB collection name to write out to csv
+               band         string, band name
+               query        Should the collection first be queried?
+        Output: None, write out lyrics df to csv file for later importation
+                      into sentiment analysis python script
+    '''
+    if query == None:
+        query = collection.find()
+
+    # Put mongo database into a dataframe and output to csv file
+    df =  pd.DataFrame(list(query))
+
+    # Outputting df to csv
+    df.to_csv('{}_lyrics.csv'.format(band))
+
+
+# def lemmatize_lyrics(lyrics, stop_words):
+#     # Remove punctutation
+#     lyrics = unicode(lyrics.translate(None, punctutation))
+#
+#     # Run the lyrics through spaCy
+#     lyrics = nlp(lyrics)
+#
+#     # Lemmatize lyrics
+#     tokens = [token.lemma_ for token in lyrics]
+#
+#     return ' '.join(word for word in tokens if word not in stop_words)
 
 
 if __name__ == '__main__':
+
     # Prompt user for band name
     band = raw_input("Enter a band name: ")
-    band = band.lower().replace(" ", "")
 
     # Calling function to create band url for azlyrics.com
-    band_url = band_url_generator(band)
+    band, band_url = band_url_generator(band)
 
     # Calling function to create song lyric url for azlyrics.com
     song_list = song_list_generator(band_url)
@@ -145,3 +231,6 @@ if __name__ == '__main__':
     # Scraping song lyrics from azlyrics.com and inserting into mongo
     #   database
     collection = scrape_lyrics(band, song_list)
+
+    # Outputting lyrics to csv file
+    # lyrics_to_csv(collection, band)

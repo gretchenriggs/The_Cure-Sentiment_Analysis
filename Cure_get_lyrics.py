fa15e6af685bba5d26321b31a106c5bd23605397
gretchenriggs <gretchen.m.riggs@gmail.com>
2017-04-10 12:02:44 -0600

    Updating script for thecure.com

--- Cure_get_lyrics.py
+++ Cure_get_lyrics.py
@@ -24,14 +24,20 @@ def era_url_generator(era_list):
     return era_url_list
 
 
-def song_list_generator(era_url_list):
+def song_list_generator(era_list):
     ''' Creating list of songs for The Cure from thecure.com
         Input: era_url_list     list of strings, urls for eras on thecure.com
         Output: song_list       list of strings, list containing all songs with
                                   lyrics for The Cure on thecure.com
     '''
+    # Calling function to create band url for thecure.com
+    era_url_list = era_url_generator(era_list)
+
     all_song_list = []
-    for era_url in era_url_list:
+    for index, era_url in enumerate(era_url_list):
+        # Saving era listing to 'era'
+        era = era_list[index]
+
         # Creating a PoolManager, which is an abstraction for a container for a
         #   collection of connections to the host
         http = urllib3.PoolManager()
@@ -62,7 +68,7 @@ def song_list_generator(era_url_list):
             all_song_list.append(temp)
 
             # Call function to grab lyrics to put into MongoDB
-            scrape_lyrics(era_url_list)
+            scrape_lyrics(era, era_url, soup)
 
             # Sleep a bit so don't get kicked out of website
             sleep(5)
@@ -76,70 +82,51 @@ def song_list_generator(era_url_list):
 
 
 
-def scrape_lyrics(era_url_list):
+def scrape_lyrics(era, era_url, soup):
     ''' Scraping lyrics from all songs listed for The Cure on thecure.com
-        Input:  era_url_list  list of strings, list of url lists for eras
+        Input:  era           string, list of years for era
+                era_url_list  list of strings, list of url lists for eras
         Output: collection    MongoDB, containing lyrics only
     '''
     # Creating mongo database_name & collection_name
     database_name = 'The_Cure'
     collection_name = 'lyrics'
 
-    # Loop through to get some lyrics for each song
-    for era in era_url_list:
+    # Find all song lyrics on the page
+    lyrics = soup.findAll("p")
 
-        # Creating a PoolManager, which is an abstraction for a container for a
-        #   collection of connections to the host
-        http = urllib3.PoolManager()
+    # Removing unneeded text from lyric listing
+    lyrics = str(lyrics).replace("\\r\\n", " ")\
+                        .replace("</br>"," ")\
+                        .replace("<br>", " ")\
+                        .replace("\\n", " ")\
+                        .replace("</div>]", "")\
+                        .replace("  ", " ")\
+                        .replace("  ", " ")\
+                        .replace("  ", " ")\
+                        .replace("<p>", "")\
+                        .replace("&amp;amp;amp;hellip;", "")\
+                        .replace("</p>", "")\
+                        .replace("\'", "")\
+                        .replace('"', "")\
+                        .replace(",", "")\
+                        .replace(".", "")\
+                        .replace("?", "")\
+                        .replace("!", "")\
+                        .lower()
 
-        # Making sure to have enough time between requests so not hitting
-        #   max requests to website
-        response = ""
-        while response == "":
-            try:
-                # Make a request to the host
-                response  = http.request('GET', lyrics_url)
-            except:
-                # Print messages to screen and sleep for 5 seconds and try again
-                print "Connection refused by the server."
-                print "Sleeping for 5 seconds."
-                sleep(5)
-                print "Continuing"
-                continue
+        # Format song and lyrics pymongo-style
+        all_lyrics = {era : lyrics}
 
-        # If request succeeded, proceed with lyric scraping
-        if response.status == 200:
+        # Insert song and lyrics into mongo database
+        db_insert_lyrics(database_name, collection_name, all_lyrics)
 
-            # Using BeautifulSoup to store era_url page as lxml
-            lyrics_soup  = bs(response.data, "lxml")
+    else:
+        error_message = "Lyrics could not be found"
+        db_insert_lyrics(database_name, collection_name, song, \
+                         error_message)
 
-            # Find all song lyrics on the page
-            lyrics = lyrics_soup.findAll("div", attrs={"class": None, \
-                                                          "id": None})
-
-            # Removing unneeded text from lyric listing
-            lyrics = str(lyrics).replace("[<div>\\n<!-- Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that. -->", "")\
-                                .replace("\\r\\n", " ")\
-                                .replace("<br / >"," ")\
-                                .replace("<br/>", " ")\
-                                .replace("\\n", " ")\
-                                .replace("</div>]", "")\
-                                .replace("  ", " ")\
-                                .lower()
-
-            # Format song and lyrics pymongo-style
-            song_lyrics = {song: lyrics}
-
-            # Insert song and lyrics into mongo database
-            # if db
-            db_insert_lyrics(database_name, collection_name, song, song_lyrics)
-
-        else:
-            error_message = "Lyrics could not be found"
-            db_insert_lyrics(database_name, collection_name, song, \
-                             error_message)
-
-        sleep(5)
+    sleep(5)
     # return collection for further QC
     collection = connect_to_mongo(database_name, collection_name)
     return collection
@@ -162,7 +149,7 @@ def connect_to_mongo(database_name, collection_name):
     return lyric_collection
 
 
-def db_insert_lyrics(database_name, collection_name, lyrics):
+def db_insert_lyrics(database_name, collection_name, all_lyrics):
     ''' Insert lyrics for song into MongoDB table
         Input: database_name    string, MongoDB database name to insert into
                collection name  string, MongoDB collection name to insert into
@@ -173,25 +160,25 @@ def db_insert_lyrics(database_name, collection_name, lyrics):
     collection = connect_to_mongo(database_name,collection_name)
 
     # Insert new song lyrics into mongo database
-    collection.insert_one({song : lyrics})
+    collection.insert_one(all_lyrics)
 
 
-def lyrics_to_csv(collection, band, query=None):
-    ''' Output lyric collection for band to csv file
-        Input: collection   string, MongoDB collection name to write out to csv
-               band         string, band name
-               query        Should the collection first be queried?
-        Output: None, write out lyrics df to csv file for later importation
-                      into sentiment analysis python script
-    '''
-    if query == None:
-        query = collection.find()
-
-    # Put mongo database into a dataframe and output to csv file
-    df =  pd.DataFrame(list(query))
-
-    # Outputting df to csv
-    df.to_csv('{}_lyrics.csv'.format(band))
+# # def lyrics_to_csv(collection, band, query=None):
+#     ''' Output lyric collection for band to csv file
+#         Input: collection   string, MongoDB collection name to write out to csv
+#                band         string, band name
+#                query        Should the collection first be queried?
+#         Output: None, write out lyrics df to csv file for later importation
+#                       into sentiment analysis python script
+#     '''
+#     if query == None:
+#         query = collection.find()
+#
+#     # Put mongo database into a dataframe and output to csv file
+#     df =  pd.DataFrame(list(query))
+#
+#     # Outputting df to csv
+#     df.to_csv('{}_lyrics.csv'.format(band))
 
 
 # def lemmatize_lyrics(lyrics, stop_words):
@@ -212,15 +199,13 @@ if __name__ == '__main__':
     #   kicking me out for too many requests)
     era_list = ['1978-1979', '1980', '1981-1982', '1983-1984', '1985-1987', '1988-1990', '1991-1993', '1994-2004', '2005-2009']
 
-    # Calling function to create band url for thecure.com
-    era_url_list = era_url_generator(era_list)
-
-    # Calling function to create song lyric url for azlyrics.com
-    song_list = song_list_generator(era_url_list)
+    # Calling function to create song list from thecure.com & store lyrics
+    #   in MongoDB database by era : all_lyrics
+    song_list = song_list_generator(era_list)
 
     # Scraping song lyrics from azlyrics.com and inserting into mongo
     #   database
-    collection = scrape_lyrics(band, song_list)
+    # collection = scrape_lyrics(band, song_list)
 
     # Outputting lyrics to csv file
     # lyrics_to_csv(collection, band)

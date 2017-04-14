3efa55485247eaf06d1c32a8d81210528dac788b
gretchenriggs <gretchen.m.riggs@gmail.com>
2017-04-14 14:56:25 -0600

    Creating MongoDB with album info (title, songs, year)

--- Cure_album_info.py
+++ Cure_album_info.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup as bs
 import urllib3.request
 from collections import defaultdict
+from pymongo import MongoClient
 
 
 def get_soup(query=True):
@@ -29,31 +30,77 @@ def get_soup(query=True):
 
 
 def album_song_list_generator(soup):
-    # Grabbing all album titles from BeautifulSoup object
-    albums = soup.findAll("b")
+    # Creating mongo database_name & collection_name
+    database_name = 'The_Cure'
+    collection_name = 'album_info'
 
-    # Grabbing all song titles from BeautifulSoup object
-    songs = soup.findAll("a")[32:-10]
+    # Check if database is already populated (non-empty)
+    collection = connect_to_mongo(database_name, collection_name)
 
-    # Reformatting albums and songs to remove html elements and make lower case
-    albums = [str(album).replace("<b>", "")\
-                         .replace("</b>", "")\
-                         .replace('"','')\
-                         .lower() for album in albums]
+    if list(collection.find()) == []:
+        # Grabbing all album titles from BeautifulSoup object
+        albums = soup.findAll("b")
 
-    album_song_dict = defaultdict(list)
-#    songs = songs[1:]
-    j = 0
-    for song in songs:
-        if "href" in song:
-            song = str(song).split(">")[1]\
-            .replace("</a","")\
-            .lower()
-            print albums[j], song
-            album_song_dict[albums[j]].append(song)
-        else:
-            j+=1
-            continue
+        # Grabbing all song titles from BeautifulSoup object
+        songs = soup.findAll("a")[32:-10]
+
+        # Grabbing year for each album from BeautifulSoup object
+        years = soup.findAll("div", {"class":"album"})[0:15]
+        for year in years:
+            year = str(year).split("</b> ")[1]\
+                            .replace("(", "")\
+                            .replace(")</div>","")
+
+        # Reformatting albums and songs to remove html elements and make lower case
+        albums = [str(album).replace("<b>", "")\
+                             .replace("</b>", "")\
+                             .replace('"','')\
+                             .lower() for album in albums]
+
+        album_song_dict = defaultdict(list)
+    #    songs = songs[1:]
+        j = 0
+        for song in songs:
+            if "href" in str(song):
+                song = str(song).split(">")[1]\
+                .replace("</a","")\
+                .lower()
+                print albums[j], song
+                album_song_dict[albums[j]].append(song)
+            else:
+                j+=1
+                continue
+
+def connect_to_mongo(database_name, collection_name):
+    ''' Connect to Mongo database
+        Input: database_name      string, MongoDB database name
+               collection_name    string, MongoDB collection name
+        Output: lyric_collection  database table, connecting to MongoDB table
+    '''
+    # Initial client to connect to mongo
+    client = MongoClient()
+
+    # Access/Initialize database
+    database = client[database_name]
+
+    # Access/Initialize Table
+    lyric_collection = database[collection_name]
+    return lyric_collection
+
+
+def db_insert_album_info(database_name, collection_name, album_info):
+    ''' Insert songs and year released for albums into MongoDB table
+        Input: database_name    string, MongoDB database name to insert into
+               collection name  string, MongoDB collection name to insert into
+               album_info       string, album name, songs, year to insert into
+                                  MongoDB
+        Output: None, collection is just updated
+    '''
+    # Connect to and initialize mongo database
+    collection = connect_to_mongo(database_name,collection_name)
+
+    # Insert new song lyrics into mongo database
+    collection.insert_one(all_lyrics)
 
 
 if __name__ == '__main__':
